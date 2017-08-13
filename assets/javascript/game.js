c1a5d452ca8046d40b943defd3a921bb0cd4154b
jtmPC <jon87tyler@gmail.com>
2017-08-12 21:12:02 -0700

    Corrected some stuff when selecting an attacker.  Finished selecting opponent.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 	//Global variables
 	var increasedAttack;
 	var remainingHPAttacker;
+	var retaliate;
 	var remainingHPDefender;
 	var yourCharacterSelect = false;
 	var defenderSelect = false;
@@ -42,30 +43,98 @@ $(document).ready(function () {
 	
 	if (!yourCharacterSelect) {
 		$(".revan").on("click", function() {
-			$(".contender").css("display", "none");
+			$(".beginHere").css("display", "none");
 			$("#yourCharacter").html('<h2>Your Character</h2><div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div>');
-			$("#selectEnemy").html('<h2>Chose your opponent.  Choose wisely.</h2><div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div><div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div><div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Storm Trooper"></div><h3>Stormtrooper</h3></div>');
+			$("#opponentStarkiller").html('<div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div>');
+			$("#opponentSkywalker").html('<div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
+			$("#opponentTrooper").html('<div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Storm Trooper"></div><h3>Stormtrooper</h3></div>');
+			yourCharacterSelect = true;
+			defenderSelect = (false);
+			increasedAttack = Revan.Attack;
+			remainingHPAttacker = Revan.HP;
+			console.log("Darth Revan's Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".")
 		})
 		$(".starkiller").on("click", function() {
-			$(".contender").css("display", "none");
+			$(".beginHere").css("display", "none");
 			$("#yourCharacter").html('<h2>Your Character</h2><div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div>');
-			$("#selectEnemy").html('<h2>Chose your opponent.  Choose wisely.</h2><div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div><div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div><div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Storm Trooper"></div><h3>Stormtrooper</h3></div>');
+			$("#opponentRevan").html('<div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div>');
+			$("#opponentSkywalker").html('<div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
+			$("#opponentTrooper").html('<div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Storm Trooper"></div><h3>Stormtrooper</h3></div>');
+			yourCharacterSelect = true;
+			defenderSelect = (false);
+			increasedAttack = Starkiller.Attack;
+			remainingHPAttacker = Starkiller.HP;
+			console.log("Starkiller's Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".")
 		})
 		$(".skywalker").on("click", function() {
-			$(".contender").css("display", "none");
+			$(".beginHere").css("display", "none");
 			$("#yourCharacter").html('<h2>Your Character</h2><div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
-			$("#selectEnemy").html('<h2>Chose your opponent.  Choose wisely.</h2><div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div><div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div><div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Storm Trooper"></div><h3>Stormtrooper</h3></div>');
+			$("#opponentRevan").html('<div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div>');
+			$("#opponentStarkiller").html('<div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div>');
+			$("#opponentTrooper").html('<div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Storm Trooper"></div><h3>Stormtrooper</h3></div>');
+			yourCharacterSelect = true;
+			defenderSelect = (false);
+			increasedAttack = Skywalker.Attack;
+			remainingHPAttacker = Skywalker.HP;
+			console.log("Jedi Anakin Skywalker's Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".")
 		})
 		$(".trooper").on("click", function() {
-			$(".contender").css("display", "none");
+			$(".beginHere").css("display", "none");
 			$("#yourCharacter").html('<h2>Your Character</h2><div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Stormtrooper"></div><h3>Stormtrooper</h3></div>');
-			$("#selectEnemy").html('<h2>Chose your opponent.  Choose wisely.</h2><div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div><div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div><div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
+			$("#opponentRevan").html('<div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div>');
+			$("#opponentStarkiller").html('<div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div>');
+			$("#opponentSkywalker").html('<div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
+			yourCharacterSelect = true;
+			defenderSelect = (false);
+			increasedAttack = Trooper.Attack;
+			remainingHPAttacker = Trooper.HP;
+			console.log("The Strom Trooper's Attack Value is...unclear... His starting health is " + remainingHPAttacker + ".")
 		})
-	}
+	};
+
+	if (!defenderSelect) {
+		$("#opponentRevan").on("click", function() {
+			$("#opponentRevan").css("display", "none");
+			$("#opponent").append('<div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div>');
+			remainingHPDefender = Revan.HP;
+			retaliate = Revan.Retaliate;
+			console.log("You've chosen a powerful enemy!  Darth Revan!! He has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!  The possibility of successfully defeating this legened are approxamately  3,720 to 1!!")
+			defenderSelect = true;
+		})
+		$("#opponentStarkiller").on("click", function() {
+			$("#opponentStarkiller").css("display", "none");
+			$("#opponent").append('<div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div>');
+			remainingHPDefender = Starkiller.HP;
+			retaliate = Starkiller.Retaliate;
+			console.log("You MUST be losing your mind to take on this secret weapon of a warrior!  Starkiller!! He has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!  I've got a bad feeling about this...")
+			defenderSelect = true;
+		})
+		$("#opponentSkywalker").on("click", function() {
+			$("#opponentSkywalker").css("display", "none");
+			$("#opponent").append('<div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
+			remainingHPDefender = Skywalker.HP;
+			retaliate = Skywalker.Retaliate;
+			console.log("Seriously??  This guy was BORN of the force!! He has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!  Whatever you do, just please don't mention sand...")
+			defenderSelect = true;
+		})
+		$("#opponentTrooper").on("click", function() {
+			$("#opponentTrooper").css("display", "none");
+			$("#opponent").append('<div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Strom Trooper"></div><h3>Stormtrooper</h3></div>');
+			remainingHPDefender = Trooper.HP;
+			retaliate = Trooper.Retaliate;
+			console.log("These bucketheads don't have the best aim w a blaster...doesn't make for much of a fight, I guess. He has " + remainingHPDefender + " health.  I think I heard some rumor about these guys having some heavy explosives, though...")
+			defenderSelect = true;
+		})
+
+	};
+
+	
 
 
 
-//PSUEDO CODE ***************
+
+
+//*************** PSUEDO CODE ***************
 	//Global variables:
 		//each contender needs initial values for:
 			//Name
@@ -99,6 +168,7 @@ $(document).ready(function () {
 		//computate yourCharacter's attack against defender's remainingHP first. 
 			//display defender's newly calculated remainingHP
 			//if defender HP <= 0, no Retaliate and toggle defenderSelect.  
+				//make the defenderSelect var false again so that...
 				//user chooses new defender. (new var defender2 defender3?)
 			//else (defender lives), computate defender's Retaliate against yourCharacter's remainingHP.
 				//display yourCharacter's newly calculated remainingHP
