6c46a49d94848158039fe4ff53c50e3582eb806d
jtmPC <jon87tyler@gmail.com>
2017-08-12 23:52:56 -0700

    Completed battle logic.  Added reset button and logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -55,8 +55,8 @@ $(document).ready(function () {
 			attackerName = "Darth Revan";
 			increasedAttack = Revan.Attack;
 			remainingHPAttacker = Revan.HP;
-			$("#history").append("<h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
-			$("#history").append("<h3>Pick your opponent.</h3>");
+			$("#history").prepend("<hr><h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
+			$("#history").prepend("<h3>Pick your opponent.</h3>");
 		})
 		$(".starkiller").on("click", function() {
 			$(".beginHere").css("display", "none");
@@ -69,8 +69,8 @@ $(document).ready(function () {
 			attackerName = "Starkiller";
 			increasedAttack = Starkiller.Attack;
 			remainingHPAttacker = Starkiller.HP;
-			$("#history").append("<h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
-			$("#history").append("<h3>Pick your opponent.</h3>");
+			$("#history").prepend("<h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
+			$("#history").prepend("<h3>Pick your opponent.</h3>");
 		})
 		$(".skywalker").on("click", function() {
 			$(".beginHere").css("display", "none");
@@ -83,8 +83,8 @@ $(document).ready(function () {
 			attackerName = "Jedi Anakin Skywalker";
 			increasedAttack = Skywalker.Attack;
 			remainingHPAttacker = Skywalker.HP;
-			$("#history").append("<h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
-			$("#history").append("<h3>Pick your opponent.</h3>");
+			$("#history").prepend("<h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
+			$("#history").prepend("<h3>Pick your opponent.</h3>");
 		})
 		$(".trooper").on("click", function() {
 			$(".beginHere").css("display", "none");
@@ -97,8 +97,8 @@ $(document).ready(function () {
 			attackerName = "Stormtrooper";
 			increasedAttack = Trooper.Attack;
 			remainingHPAttacker = Trooper.HP;
-			$("#history").append("<h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
-			$("#history").append("<h3>Pick your opponent.</h3>");
+			$("#history").prepend("<h5>" + attackerName + "'s Attack Value is " + increasedAttack + " and his starting health is " + remainingHPAttacker + ".</h5>");
+			$("#history").prepend("<h3>Pick your opponent.</h3>");
 		})
 	};
 
@@ -107,46 +107,46 @@ $(document).ready(function () {
 	if (!defenderSelect) {
 		$("#opponentRevan").on("click", function() {
 			$("#opponentRevan").css("display", "none");
-			$("#opponent").append('<div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div>');
+			$("#opponent").prepend('<div class="col-xs-6 col-md-3 contender revan"><div class="contender"><img src="assets/images/Revan.jpg" alt="Darth Revan"></div><h3>Darth Revan</h3></div>');
 			defenderName = "Darth Revan";
 			remainingHPDefender = Revan.HP;
 			retaliate = Revan.Retaliate;
-			$("#history").append("<h5>You've chosen a powerful enemy! <br> <b>" + defenderName + "!! He has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!</b><br>  The possibility of successfully defeating this legened are approxamately  3,720 to 1!!</h5>")
+			$("#history").prepend("<h5>You've chosen a powerful enemy! <br> <b>" + defenderName + "!! He has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!</b><br>  The possibility of successfully defeating this legened are approxamately  3,720 to 1!!</h5>")
 			defenderSelect = true;
-			$("#history").append("<h3>Click 'Attack' to proceed.</h3>");
+			$("#history").prepend("<h3>Click 'Attack' to proceed.</h3>");
 			$("#fightSection").css("display", "block");
 		})
 		$("#opponentStarkiller").on("click", function() {
 			$("#opponentStarkiller").css("display", "none");
-			$("#opponent").append('<div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div>');
+			$("#opponent").prepend('<div class="col-xs-6 col-md-3 contender starkiller"><div class="contender"><img src="assets/images/starkiller.jpg" alt="Starkiller"></div><h3>Starkiller</h3></div>');
 			defenderName = "Starkiller";
 			remainingHPDefender = Starkiller.HP;
 			retaliate = Starkiller.Retaliate;
-			$("#history").append("<h5>You MUST be losing your mind to take on this secret weapon of a warrior!<br> <b>" + defenderName + "!! He has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!</b><br> I've got a bad feeling about this...")
+			$("#history").prepend("<hr><h5>You MUST be losing your mind to take on this secret weapon of a warrior!<br> <b>" + defenderName + "!! He has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!</b><br> I've got a bad feeling about this...")
 			defenderSelect = true;
-			$("#history").append("<h3>Click 'Attack' to proceed.</h3>");
+			$("#history").prepend("<h3>Click 'Attack' to proceed.</h3>");
 			$("#fightSection").css("display", "block");
 		})
 		$("#opponentSkywalker").on("click", function() {
 			$("#opponentSkywalker").css("display", "none");
-			$("#opponent").append('<div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
+			$("#opponent").prepend('<div class="col-xs-6 col-md-3 contender skywalker"><div class="contender"><img src="assets/images/anakin.jpg" alt="Jedi Anakin Skywalker"></div><h3>Jedi Anakin Skywalker</h3></div>');
 			defenderName = "Jedi Anakin Skywalker";
 			remainingHPDefender = Skywalker.HP;
 			retaliate = Skywalker.Retaliate;
-			$("#history").append("<h5>Seriously??  This guy was BORN of the force!!<br> <b>" + defenderName + " has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!</b><br>  Just <b><i>please</i></b> don't mention sand...he never shuts up about it...</h5>")
+			$("#history").prepend("<h5>Seriously??  This guy was BORN of the force!!<br> <b>" + defenderName + " has " + remainingHPDefender + " health and a fearsome " + retaliate + " counter attack!!</b><br>  Just <b><i>please</i></b> don't mention sand...he never shuts up about it...</h5>")
 			defenderSelect = true;
-			$("#history").append("<h3>Click 'Attack' to proceed.</h3>");
+			$("#history").prepend("<h3>Click 'Attack' to proceed.</h3>");
 			$("#fightSection").css("display", "block");
 		})
 		$("#opponentTrooper").on("click", function() {
 			$("#opponentTrooper").css("display", "none");
-			$("#opponent").append('<div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Strom Trooper"></div><h3>Stormtrooper</h3></div>');
+			$("#opponent").prepend('<div class="col-xs-6 col-md-3 contender trooper"><div class="contender"><img src="assets/images/trooper.jpg" alt="Strom Trooper"></div><h3>Stormtrooper</h3></div>');
 			defenderName = "Stormtrooper";
 			remainingHPDefender = Trooper.HP;
 			retaliate = Trooper.Retaliate;
-			$("#history").append("<h5>These bucketheads don't have the best aim w a blaster...doesn't make for much of a fight, I guess.<br> <b>The " + defenderName + " has " + remainingHPDefender + " health.  I don't see anything here about his counter attack though. Strage.</b><br>  I think I heard some rumor about these guys having some heavy explosives, though...</h5>")
+			$("#history").prepend("<h5>These bucketheads don't have the best aim w a blaster...doesn't make for much of a fight, I guess.<br> <b>The " + defenderName + " has " + remainingHPDefender + " health.  I don't see anything here about his counter attack though. Strage.</b><br>  I think I heard some rumor about these guys having some heavy explosives, though...</h5>")
 			defenderSelect = true;
-			$("#history").append("<h3>Click 'Attack' to proceed.</h3>");
+			$("#history").prepend("<h3>Click 'Attack' to proceed.</h3>");
 			$("#fightSection").css("display", "block");
 		})
 
@@ -160,29 +160,58 @@ $(document).ready(function () {
 			remainingHPAttacker -= retaliate;
 			console.log(attackerName + remainingHPAttacker);
 			if ((remainingHPAttacker > 0) && (remainingHPDefender > 0)) {
-				$("#history").append("<h5>Good job. " + attackerName + " did " + increasedAttack + " damage to " + defenderName + ", though " + defenderName + " did " + retaliate + " damage to" + attackerName + ".</h5>");
-				$("#history").append("<h5>" + attackerName + "'s health: " + remainingHPAttacker + "</h5>");
-				$("#history").append("<h5>" + defenderName + "'s health: " + remainingHPDefender + "</h5>");
-				$("#history").append("<h3>Click 'Attack' to proceed.</h3>");
+				$("#history").prepend("<h5>Good job. " + attackerName + " did " + increasedAttack + " damage to " + defenderName + ", though " + defenderName + " did " + retaliate + " damage to" + attackerName + ".</h5>");
+				$("#history").prepend("<h5>" + attackerName + "'s health: " + remainingHPAttacker + "</h5>");
+				$("#history").prepend("<h5>" + defenderName + "'s health: " + remainingHPDefender + "</h5>");
+				$("#history").prepend("<hr><h3>Click 'Attack' to proceed.</h3>");
 				increasedAttack *= 2;
 				console.log(attackerName + increasedAttack);
 			} 
 			else if ((remainingHPAttacker <= 0) && (remainingHPDefender > 0)) {
-				$("#history").append("<h5>Uh oh..." + attackerName + "'s last attach wasn't enough. " + attackerName + " did " + increasedAttack + " damage, though " + defenderName + " did " + retaliate + " damage and killed you! " + attackerName + "'s health fell below 0 while " + defenderName + " lives to fight another day.</h5>");
-				$("#history").append("<h3>Click 'Reset' to start anew.</h3>");
+				$("#history").prepend("<h5>Uh oh..." + attackerName + "'s last attach wasn't enough. " + attackerName + " did " + increasedAttack + " damage, though " + defenderName + " did " + retaliate + " damage and killed you! " + attackerName + "'s health fell below 0 while " + defenderName + " lives to fight another day.</h5>");
+				$("#history").prepend("<h3>Click 'Reset' to start anew.</h3>");
+				$("#fightSection").css("display", "none");
+				$(".reset").css("display", "block");
 			}
 			else if ((remainingHPAttacker <= 0) && (remainingHPDefender <= 0)) {
-				$("#history").append("<h5>" + increasedAttack + " managed to slay " + defenderName + ", but died in the act.  Both characters died!</h5>");
-				$("#history").append("<h3>Click 'Reset' to start anew.</h3>");
+				$("#history").prepend("<h5>" + attackerName + " managed to slay " + defenderName + ", but died in the act.  Both characters died!</h5>");
+				$("#history").prepend("<h3>Click 'Reset' to start anew.</h3>");
+				$("#fightSection").css("display", "none");
+				$(".reset").css("display", "block");
 			}
 			else if ((remainingHPAttacker > 0) && (remainingHPDefender <= 0)) {
-				$("#history").append("<h5>You've actually done it! " + attackerName + " did " + increasedAttack + " damage to " + defenderName + ", landing the killing blow!</h5>");
-				$("#history").append("<h3>Choose your next opponent to attack!</h3>");
+				$("#history").prepend("<h5>You've actually done it! " + attackerName + " did " + increasedAttack + " damage to " + defenderName + ", landing the killing blow!</h5>");
+				$("#history").prepend("<h3>Choose your next opponent to attack!</h3>");
+				$("#fightSection").css("display", "none");
 				defenderSelect = false;
 			}
 
 		}
 	});
+
+
+	//Reset button
+	$(".resetBTN").on("click", function() {
+		var attackerName = "";
+		var increasedAttack;
+		var remainingHPAttacker;
+		var defenderName = "";
+		var retaliate;
+		var remainingHPDefender;
+		var yourCharacterSelect = false;
+		var defenderSelect = false;
+		$("#history").html("<h2>Your Adventure:</h2><h3>(Pick your character.)</h3>")
+		$(".beginHere").css("display", "block");
+		$("#yourCharacter").html("<h2>Your Character</h2>");
+		$("#opponentRevan").empty();
+		$("#opponentStarkiller").empty();
+		$("#opponentSkywalker").empty();
+		$("#opponentTrooper").empty();
+		$("#opponent").html("<h2>Your Opponent</h2>");
+		$(".reset").css("display", "none");
+	});
+
+
 
 
 	
